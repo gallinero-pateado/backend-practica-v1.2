6ffd8b62e7d742091eba7f37a032cda53569ff95
JaimeFigueroaP22 <jfigueroap@utem.cl>
2024-10-02 14:31:44 -0300

    pruebaregister

--- api/routes.go
+++ api/routes.go
@@ -3,14 +3,20 @@ package api
 import (
 	"practica/internal/auth"
 
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/mux"
 )
 
 func SetupRoutes() *mux.Router {
 	router := mux.NewRouter()
+	authRoutes := r.Group("/")
 
 	// Ruta para autenticación con Firebase
 	router.HandleFunc("/verify", auth.VerifyHandler).Methods("POST")
+
+	authRoutes.POST("/register", func(c *gin.Context) {
+		controllers.RegisterUser(c, firestoreClient)
+	})
 
 	return router
 }
