dd9fbb18ec72cc67c610ef96ff604ec5f0310c05
xsilvamo <aaronsilva.csl@gmail.com>
2024-10-05 02:09:38 -0300

    Configuracion de CORS en PORT:3000

--- api/routes.go
+++ api/routes.go
@@ -4,10 +4,21 @@ import (
 	"practica/internal/auth"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func SetupRoutes() *gin.Engine {
 	router := gin.Default()
+
+
+	// Configurar CORS
+    router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000"}, // Cambia el puerto si es necesario
+        AllowMethods:     []string{ }, // Métodos permitidos
+        AllowHeaders:     []string{"Content-Type"},
+        AllowCredentials: true,
+    }))
+
 
 	router.POST("/register", auth.RegisterHandler)
 	router.POST("/login", auth.LoginHandler)
